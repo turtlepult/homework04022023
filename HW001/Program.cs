5cb375f9d86d6208153937a5a822c2ecf3cbd9a0
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-04 20:08:39 +0500

    perhaps a working program has been created 002

---
+++ HW001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] CreateMatrix (int m, int n)
+{
+   Random random = new Random();
+   double[,] matrix = new double[m, n];
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]=random.NextDouble()*20;
+        }
+   }
+   return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int m = ReadInt("enter amount of colums: ");
+int n = ReadInt("enter amount of n: ");
+double[,] matrix = CreateMatrix(m,n);
+PrintMatrix(matrix);
---
+++ HW002/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m,n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,10);
+        }
+    }
+return matrix;
+}
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+void PrintMatrix (int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {    for (int j = 0; j < matrix.GetLength(1); j++)
+         {
+           Console.Write(matrix[i,j]+"\t");
+         }
+         Console.WriteLine(); 
+    }
+}
+int? FiendValue (int[,] matrix, int fristIndex, int secondIndex)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == fristIndex && j==secondIndex)
+            {
+                return matrix[i,j];
+            }
+        }
+    }
+    return null;
+}
+int m = ReadInt("введите m: ");
+int n = ReadInt("Введите n: ");
+int[,] matrix = CreateMatrix(m,n);
+Console.WriteLine("Ваша матрица: ");
+PrintMatrix(matrix);
+int fristIndex = ReadInt("Введите нидекс строки:");
+int secondIndex = ReadInt("Введите индекс столбца: ");
+Console.Write("Число стоящее на позиции ["+fristIndex+", "+secondIndex+"]: "+ FiendValue(matrix, fristIndex, secondIndex));
